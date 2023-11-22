e0822d1022e4b4e3fc5f6fa055f9991c46990e45
Lucas Aramberry <lucas03aramberry@gmail.com>
2023-11-22 00:04:07 -0300

    Control Clientes

---
+++ ControlClientes/src/main/java/com/controlclientes/dao/PersonaDao.java
@@ -0,0 +1,8 @@
+package com.controlclientes.dao;
+
+import com.controlclientes.domain.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonaDao extends JpaRepository<Persona, Long>{
+    
+}
---
+++ ControlClientes/src/main/java/com/controlclientes/dao/UsuarioDao.java
@@ -0,0 +1,8 @@
+package com.controlclientes.dao;
+
+import com.controlclientes.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioDao extends JpaRepository<Usuario, Long>{
+    Usuario findByUsername(String username);
+}
